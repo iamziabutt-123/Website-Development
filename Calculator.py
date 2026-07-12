0db6edad3503ec556328eb4669975622bc801333
iamziabutt-123 <iamziabutt@gmail.com>
2026-07-12 16:06:05 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+def add(p,q):
+    return p+q
+def subtract(p,q):
+    return p-q
+def multiply(p,q):
+    return p*q
+def divide(p,q):
+    return p/q
+
+num1 = int(input("Enter the number:"))
+num2 = int(input("Enter the number:"))
+
+print(add(num1,num2))
+print(subtract(num1,num2))
+print(multiply(num1,num2))
+print(divide(num1,num2))
